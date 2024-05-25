2f8a237644e951d6fc501caab1ab48d51f2a2918
Sviatoslav2013076 <svchsv73@gmail.com>
2024-05-25 10:40:30 +0300

    Add files via upload

---
+++ kalkuliator python.py
@@ -0,0 +1,24 @@
+num1 = int(input("comment:"))
+
+num2 = int(input("comment2:"))
+
+dodavania = "(1 dodavania"
+
+vidnimania = "(2 vidnimania"
+
+mnozhenia = "(3 mnozhenia"
+
+dilenia = "(4 dilenia "
+
+print(dodavania)
+
+print(vidnimania)
+
+print(mnozhenia)
+
+print(dilenia)
+
+print("resultats:", num1 + num2, " dodavania")
+print("resultats:", num1 - num2,  " vidnimania")
+print("resultats:", num1 * num2, " mnozhenia")
+print("resultats:", num1 / num2, " dilenia")
